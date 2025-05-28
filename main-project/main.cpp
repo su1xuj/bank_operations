f848356fc46f2099773f69bfbbbbf62e722736d6
su1xuj <125282000+su1xuj@users.noreply.github.com>
2025-05-28 16:44:05 +0300

    Добавлен вывод общей информации о задании

--- main-project/main.cpp
+++ main-project/main.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 
+using namespace std;
+
 int main()
 {
-    std::cout << "Hello\n";
+    cout << "Laboratory work #8. GIT\n";
+    cout << "Variant #0. Library Subscription\n";
+    cout << "Author: Sergey Yermochenko\n";
     return 0;
 }
