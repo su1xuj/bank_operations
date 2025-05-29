ec7a756528b92183d6b5e7886c2cdf281bfa6e2a
su1xuj <su1xuj@mail.ru>
2025-05-29 11:45:23 +0300

    жетко добавил тесты

---
+++ unit-test-project/unit-test-project.cpp
@@ -0,0 +1,77 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../main-project/bank_operations.h" // Путь к заголовочному файлу
+#include "../main-project/processing.h" // Путь к заголовочному файлу с функцией process
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace unittestproject
+{
+    // Функция для создания операции
+    BankOperation* build_operation(const std::string& type, double amount)
+    {
+        BankOperation* operation = new BankOperation;
+        strncpy_s(operation->type, type.c_str(), sizeof(operation->type) - 1);
+        operation->type[sizeof(operation->type) - 1] = '\0'; // гарантируем нуль-терминатор
+        operation->amount = amount;
+        return operation;
+    }
+
+    // Функция для удаления массива операций
+    void delete_operations(BankOperation* array[], int size)
+    {
+        for (int i = 0; i < size; i++)
+        {
+            delete array[i];
+        }
+    }
+
+    TEST_CLASS(unittestproject)
+    {
+    public:
+        TEST_METHOD(TestIncomeOperation)
+        {
+            BankOperation* operations[2];
+            operations[0] = build_operation("income", 100.0);
+            operations[1] = build_operation("Expense", 50.0);
+            Assert::AreEqual(50.0, process(operations, 2));
+            delete_operations(operations, 2);
+        }
+
+        TEST_METHOD(TestMultipleOperations)
+        {
+            BankOperation* operations[3];
+            operations[0] = build_operation("income", 200.0);
+            operations[1] = build_operation("Expense", 50.0);
+            operations[2] = build_operation("Expense", 30.0);
+            Assert::AreEqual(120.0, process(operations, 3));
+            delete_operations(operations, 3);
+        }
+
+        TEST_METHOD(TestNoOperations)
+        {
+            BankOperation** operations = nullptr; // Используем nullptr
+            Assert::AreEqual(0.0, process(operations, 0));
+        }
+
+        TEST_METHOD(TestOnlyExpenseOperations)
+        {
+            BankOperation* operations[2];
+            operations[0] = build_operation("Expense", 100.0);
+            operations[1] = build_operation("Expense", 50.0);
+            Assert::AreEqual(-150.0, process(operations, 2));
+            delete_operations(operations, 2);
+        }
+
+        TEST_METHOD(TestMixedOperations)
+        {
+            BankOperation* operations[4];
+            operations[0] = build_operation("income", 300.0);
+            operations[1] = build_operation("Expense", 100.0);
+            operations[2] = build_operation("income", 200.0);
+            operations[3] = build_operation("Expense", 50.0);
+            Assert::AreEqual(350.0, process(operations, 4));
+            delete_operations(operations, 4);
+        }
+    };
+}
