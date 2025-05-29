1aa33d3f585b28e1a515df22d7574865cbcc98e6
su1xuj <su1xuj@mail.ru>
2025-05-29 12:04:48 +0300

    финал верия суммы

--- main-project/process.cpp
+++ main-project/process.cpp
@@ -13,7 +13,7 @@ double process(BankOperation* array[], int size) {
     double total = 0.0;
 
     for (int i = 0; i < size; i++) {
-        if (myStrEqual(array[i]->type, "Inome")) {
+        if (myStrEqual(array[i]->type, "Income")) {
             total += array[i]->amount;
         }
         else if (myStrEqual(array[i]->type, "Expense")) {
