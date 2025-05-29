60be966ca186b6e249dc12e0ee3101aeff6a601e
su1xuj <su1xuj@mail.ru>
2025-05-29 10:49:29 +0300

    добавил что-то из 5 лабы про процессы

---
+++ main-project/process.cpp
@@ -0,0 +1,25 @@
+#include "processing.h"
+
+// Функция для обработки банковских операций и вычисления текущей суммы на счете
+bool myStrEqual(const char* a, const char* b) {
+    while (*a && *b) {
+        if (*a != *b) return false;
+        a++; b++;
+    }
+    return *a == '\0' && *b == '\0';
+}
+
+double process(BankOperation* array[], int size) {
+    double total = 0.0;
+
+    for (int i = 0; i < size; i++) {
+        if (myStrEqual(array[i]->type, "Inome")) {
+            total += array[i]->amount;
+        }
+        else if (myStrEqual(array[i]->type, "Expense")) {
+            total -= array[i]->amount;
+        }
+    }
+
+    return total;
+}
---
+++ main-project/processing.h
@@ -0,0 +1,8 @@
+#ifndef PROCESSING_H
+#define PROCESSING_H
+
+#include "bank_operations.h"
+
+double process(BankOperation* array[], int size);
+
+#endif
